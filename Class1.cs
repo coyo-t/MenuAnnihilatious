248e825859197c0fa0b43d09833ade799b32b59a
The Birthday Coyote :] <constachugga@gmail.com>
2025-04-16 20:06:22 -0700

    s

--- Class1.cs
+++ Class1.cs
@@ -1,5 +1,45 @@
-﻿namespace MenuAnnihilatious;
+﻿
+namespace MenuAnnihilatious;
 
-public class Class1
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[KSPAddon(KSPAddon.Startup.MainMenu, false)]
+public class Unfuckitizer : MonoBehaviour
 {
+	private bool triggered = false;
+	
+	private void LateUpdate()
+	{
+		if (!triggered)
+		{
+			Debug.Log("COYOTE TIME!!!!!!");
+			var menuEnv = FindObjectOfType<MainMenuEnvLogic>();
+			
+			if (menuEnv is not null)
+			{
+				Debug.Log(menuEnv);
+				foreach (var thingArea in menuEnv.areas)
+				{
+					Debug.LogFormat("\t{0}", thingArea);
+					DestroyImmediate(thingArea);
+				}
+			}
+			triggered = true;
+		}
+	}
+
+	// private Boolean GetMenu(out GameObject outs)
+	// {
+	// 	var maybeMenu = GameObject.Find("MainMenu");
+	// 	if (maybeMenu == null)
+	// 	{
+	// 		// This Is Stupid
+	// 		outs = dummyGameObject;
+	// 		return false;
+	// 	}
+	// 	
+	// 	outs = maybeMenu;
+	// 	return true;
+	// }
 }
